c3d1d7e4f095e895f774c2b751497f3cb362a0ba
Daniel Hyres <danhyres@gmail.com>
2022-09-05 14:05:48 -0400

    building controllers

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,1 +1,38 @@
 const schemas = require('../models/schemas.js')
+
+//getMenuIndexPage, editMenu, deleteMenu, saveMenu, newMenu
+
+module.exports = {
+  getMenuIndexPage:  (req, res) => {
+    res.render('index', {title: 'Menu Items'})
+  },
+  editMenu: async (req, res) => { //async because talking to DB
+    let session = req.session
+
+    if(!session.loggedIn){ //if NOT logged in
+      res.render('menu', {title: 'Edit', loggedIn: false, error: 'Invalid Request' })
+    } else {
+      let id = req.params.id
+      let err = ''
+
+      let menu = schemas.menu //menu schema from model
+      
+      let qry = {_id:id}
+
+      let itemResult = await menu.find(qry)
+      .then( (itemData) => {
+        if (itemData === null) {
+          err = "Invalid ID"
+        }
+
+        res.render('menu', {title: 'Edit Menu', loggedIn: session, error: err })
+      
+      })
+
+    }
+
+  },
+
+
+
+}
