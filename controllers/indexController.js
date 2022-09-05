f6883f5da045bd94099987a6db994c7be130a348
Daniel Hyres <danhyres@gmail.com>
2022-09-05 12:51:21 -0400

    building controllers

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -6,12 +6,35 @@ const schemas = require('../models/schemas.js') //this is importing the 'mySchem
 module.exports = {
   getHomePage: async (req, res) => {
     let menu = schemas.menu  //going inside the (imported) schemas object, and grabbing the menu propert
-    let session = req.session
+    let session = req.session //session data
 
     let menuResult = await menu.find({})
     .then((menuData) => {
-      res.render('index', {title: 'Menu Tracker', data: menuData, search: '', loggedIn: session.loggedIn})
+      res.render('index', {title: 'Menu Tracker', data: menuData, search: '', loggedIn: session.loggedIn}) //so these are properties for the page since we are using EJS
     })
   },
-  getSearch:
+
+  getSearch: async (req, res) => {
+    let menu = schemas.menu  
+    let session = req.session
+
+    let q = req.body.searchInput
+    let menuData = null
+
+    let qry = {name: {$regex: '^' + q, $options: 'i'}}  //'^' means only look for things that START with query and i is 'ignore casing' according to chat
+
+    if( q != null){ //if q var NOT null
+      let menuResult = await menu.find(qry) //hey mongoose, in menu collecion in DB please find anything that matches our qry (starts with and ignores case)
+      .then( (data) => {
+        menuData = data //assign data returned to this variable
+      })
+    } else {
+      q = 'Search'
+      let menuResult = await menu.find({}) 
+      .then( (data) => {
+        menuData = data 
+      })
+    }
+    res.render('index', {title: 'Menu Tracker', data: menuData, search: q, loggedIn: session.loggedIn})
+  }
 }
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,1 +1,19 @@
 const schemas = require('../models/schemas.js')
+const bcrypt = require('bcrypt') //this is obscuring passwords i.e. not storing plaintext in DB
+
+//getLoginPage, getSignupPage, getLogout, postLogin, postSignup
+
+exports.getLoginPage = (req, res) => {
+  res.render('login', {title: 'Login', loggedIn: false, error: null })
+}
+
+exports.getSignupPage = (req, res) => {
+  res.render('new-acct', {title: 'New Account', loggedIn: false, error: null })
+}
+
+exports.getLogout = (req, res) => {
+  req.session.destroy() //destroys session
+  res.redirect('/')
+}
+
+
